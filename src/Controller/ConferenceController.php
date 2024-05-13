b174dc2727622d89a5525fea63938162df746d50
Tev969 dur <freezuka14@gmail.com>
2024-05-13 12:28:03 +0000

    Easter egg

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\MakerBundle\Util\UseStatementGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,15 +11,17 @@ use Symfony\Component\Routing\Attribute\Route;
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-
+        $greet = '';
+                if ($name = $request->query->get('hello')) {
+                    $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+                }
         return new Response(<<<EOF
         
                     <html>
-                      <body><img src="/images/under-construction.gif" /></body>
+                    <body>$greet<img src="/images/under-construction.gif" /></body>
                    </html>
            EOF);
     }
 }
-UseStatementGenerator
