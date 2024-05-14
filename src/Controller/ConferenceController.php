fdadb42417dee311ebc46f78a6b72ff3a7a8f4e7
Tev969 dur <freezuka14@gmail.com>
2024-05-14 14:11:42 +0000

    Route modifier et page twig

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,26 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Twig\Environment;
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\MakerBundle\Util\UseStatementGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\MakerBundle\Util\UseStatementGenerator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(Request $request): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
-        $greet = '';
-                if ($name = $request->query->get('hello')) {
-                    $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-                }
-        return new Response(<<<EOF
-        
-                    <html>
-                    <body>$greet<img src="/images/under-construction.gif" /></body>
-                   </html>
-           EOF);
+        return new Response($twig->render('conference/index.html.twig', [
+                        'conferences' => $conferenceRepository->findAll(),
+                    ]));
     }
+
+    #[Route('/conference/{id}', name: 'conference')]
+        public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
+        {
+            return new Response($twig->render('conference/show.html.twig', [
+                'conference' => $conference,
+                'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+            ]));
+        }
 }
